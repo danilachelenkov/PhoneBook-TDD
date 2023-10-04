fd7958f48621998e9d81c5a911710e0e400f4eab
DanilaChelenkov <chelenkov.dv@gmail.com>
2023-10-04 20:57:37 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> memoryBook;
+
+    public PhoneBook() {
+        this.memoryBook = new HashMap<>();
+    }
+
     public int add(String phoneNumber,String fio) {
-        return 0;
+        memoryBook.put(phoneNumber,fio);
+        return memoryBook.size();
     }
 }
