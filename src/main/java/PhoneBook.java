9a49a6bf27b6f71856ba31475ef87d455914e376
DanilaChelenkov <chelenkov.dv@gmail.com>
2023-10-04 20:54:10 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,5 @@
 public class PhoneBook {
+    public int add(String phoneNumber,String fio) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    private PhoneBook phoneBook;
+
+    @BeforeEach
+    public void beforeEachTest() {
+        System.out.println("Инициализация экземпляра PhoneBook");
+        phoneBook = new PhoneBook();
+    }
+
+    @AfterEach
+    public void afterEachTest() {
+        System.out.println("Присваиваем null ссылку для экземпляра после каждого теста");
+        phoneBook = null;
+    }
+
+    @Test
+    public void test_AddNewEntity() {
+        System.out.println("Проверка добавления записи в PhoneBook");
+        int expected = 1;
+        int result = phoneBook.add("+79260259874","СеменовАР");
+        Assertions.assertEquals(expected, result);
+    }
 }
